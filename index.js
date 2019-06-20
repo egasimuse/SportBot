5df0ba2ebe3fe02b088143aa5bef4da71d21312c
Yavor Stoimenov <yavor.stoimenov@gmail.com>
2019-06-20 11:04:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bot = require('./oddsring');
+const fs = require('fs');
 const league = [
-    '281474976711241-European-Championship-U21',
+    '281474976710874-Americas-Copa-America',
     '281474976710937-Finland-Kakkonen-Group-A',
     '281474976710938-Finland-Kakkonen-Group-B',
     '281474976710939-Finland-Kakkonen-Group-C',
@@ -8,12 +9,18 @@ const league = [
 
 (async () => {
     await bot.initialize('https://www.oddsring.com/');
+    const allBotData = [];
     for ( let i = 0; i < league.length; i++){
         console.log('********************************' + league[i] + "*****************************");
         console.log("\n");
         let results = await bot.getResults(league[i]);
-        
-    }  
+        allBotData.push(results);
+    }
+    fs.writeFile(
+        './json/result.json',
+        JSON.stringify(allBotData, null, 2),
+        (err) => err ? console.log('Data not written: ', err) : console.log('Data written')
+    );  
 })();
 
 
